superboo07/drop-app

Add setting to quit instead of minimizing to tray on close
Adds a "Quit when closing window" toggle under General settings.
Disabled by default to preserve existing behavior (hide to tray).
When enabled, both the window's close-requested event and the
subsequent app exit-requested event skip the tray-preserving logic,
so the window closing actually quits the app instead of leaving a
tray icon behind.

Co-Authored-By: Claude Sonnet 5 <noreply@anthropic.com>

src-tauri/src/lib.rs
@@ -425,10 +425,13 @@ pub fn run() {
         })
         .on_window_event(|window, event| {
             if let WindowEvent::CloseRequested { api, .. } = event {
-                run_on_tray(|| {
-                    window.hide().expect("Failed to close window in tray");
-                    api.prevent_close();
-                });
+                let quit_on_close = borrow_db_checked().settings.quit_on_close;
+                if !quit_on_close {
+                    run_on_tray(|| {
+                        window.hide().expect("Failed to close window in tray");
+                        api.prevent_close();
+                    });
+                }
             }
         })
         .build(tauri::generate_context!())
@@ -436,11 +439,14 @@ pub fn run() {
 
     app.run(|_app_handle, event| {
         if let RunEvent::ExitRequested { code, api, .. } = event {
-            run_on_tray(|| {
-                if code.is_none() {
-                    api.prevent_exit();
-                }
-            });
+            let quit_on_close = borrow_db_checked().settings.quit_on_close;
+            if !quit_on_close {
+                run_on_tray(|| {
+                    if code.is_none() {
+                        api.prevent_exit();
+                    }
+                });
+            }
         }
     });
 }

superboo07/drop

desktop/src-tauri/src/lib.rs
@@ -453,33 +453,34 @@ pub fn run() {
         })
         .on_window_event(|window, event| {
             if let WindowEvent::CloseRequested { api, .. } = event {
-                run_on_tray(|| {
-                    // Pause webview rendering before hiding to prevent idle CPU usage
-                    #[cfg(target_os = "linux")]
-                    {
-                        let _ = window.app_handle().emit("window_visibility_change", ());
-                    }
+                let quit_on_close = borrow_db_checked().settings.quit_on_close;
+                if !quit_on_close {
+                    run_on_tray(|| {
+                        // Pause webview rendering before hiding to prevent idle CPU usage
+                        #[cfg(target_os = "linux")]
+                        {
+                            let _ = window.app_handle().emit("window_visibility_change", ());
+                        }
 
-                    window.hide().expect("Failed to hide window in tray");
-                    api.prevent_close();
-                });
+                        window.hide().expect("Failed to hide window in tray");
+                        api.prevent_close();
+                    });
+                }
             }
         })
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
 
     app.run(|_app_handle, event| {
-        if let RunEvent::ExitRequested {
-            code,
-            api,
-            ..
-        } = event
-        {
-            run_on_tray(|| {
-                if code.is_none() {
-                    api.prevent_exit();
-                }
-            });
+        if let RunEvent::ExitRequested { code, api, .. } = event {
+            let quit_on_close = borrow_db_checked().settings.quit_on_close;
+            if !quit_on_close {
+                run_on_tray(|| {
+                    if code.is_none() {
+                        api.prevent_exit();
+                    }
+                });
+            }
         }
     });
 }